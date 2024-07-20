a41a3aeadbeb8ac5ebfba82655c4fe43f695997c
Sourin <iamsourinsarkar@gmail.com>
2024-07-20 09:07:36 +0530

    Learned different iteration tools

--- iteration_tools/another.py
+++ iteration_tools/another.py
@@ -1,4 +1,23 @@
-import time
-print("Another is here")
-username = "sourin"
-print(username)
+# import time
+# print("Another is here")
+# username = "sourin"
+# print(username)
+
+# ------------------------------------- 
+
+# def sum_all(chai):
+#     return sum(chai)
+
+# print(sum_all(1,2,4))
+
+# -------------------------------------
+ 
+# def print_kwargs(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key}: {value}")
+
+# print_kwargs(name = "Shaktiman", power="Wind")
+# print_kwargs(name = "Shaktiman")
+
+# -------------------------------------
+
