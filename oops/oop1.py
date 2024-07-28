d5396ac5edd77628fd38ef71ed62f4f9cc8d80e6
Sourin <iamsourinsarkar@gmail.com>
2024-07-28 16:29:08 +0530

    Learned: Polymorphism, encapsulation

--- oops/oop1.py
+++ oops/oop1.py
@@ -1,16 +1,41 @@
 class Car:
     def __init__(self, userbrand, usermodel):
-        self.brand = userbrand
+        self.__brand = userbrand
         self.model = usermodel
+
+    def get_brand(self):
+        return self.__brand + '!'
+
+    def full_name(self):
+        return f"Brand: {self.__brand} \n {self.model}"
+
+    def fuel_type(self):
+        return "Petrol or Diesel"
+    
+    # @staticmethod
+    def general_discription():
+        return "Cars on means transport."
 
 class ECar(Car):
     def __init__(self, brand, model, batterySize):
         super().__init__(brand, model)
         self.batterySize = batterySize
+    
+    def fuel_type(self):
+        return "Electric Charge"
 
 myTesla = ECar("Tesla", "Model S", "85kwh")
-print(myTesla.model)
-print(myTesla.batterySize)
+print(myTesla.fuel_type())
+
+myCar = Car("Mahindra", "Thar")
+print(myCar.fuel_type())
+
+# print(myCar.general_discription());
+print(Car.general_discription());
+
+# print(myTesla.model)
+# print(myTesla.batterySize)
+# print(myTesla.get_brand())
 
 # my_car = Car("Kia", "Seltos")
 # print(my_car.brand)
